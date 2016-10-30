7fb43120328abbce2fa27899503a9dc26ff91303
PJayRushton <parker.rushton@octanner.com>
2016-10-30 11:55:39 -0600

    Renames to ClassListVC

---
+++ TeacherTools/ClassListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClassListViewController.swift
+//  TeacherTools
+//
+//  Created by Parker Rushton on 10/30/16.
+//  Copyright © 2016 AppsByPJ. All rights reserved.
+//
+
+import UIKit
+
+class ClassListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
