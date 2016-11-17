f0cbbe3b1346e4df13fe6658ba98b0732fcbc1f3
PJayRushton <parker.rushton@octanner.com>
2016-11-16 20:02:57 -0700

    Gets name draw working

--- TeacherTools/NameDrawViewController.swift
+++ TeacherTools/NameDrawViewController.swift
@@ -10,9 +10,79 @@ import UIKit
 
 class NameDrawViewController: UIViewController, AutoStoryboardInitializable {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate let cellReuseIdentifier = "NameDrawCell"
+    fileprivate var allStudents = [Student]()
+    fileprivate var selectedStudents = [Student]()
+    fileprivate var currentStudent: Student? {
+        didSet {
+            handleNewStudent(currentStudent, previousStudent: oldValue)
+        }
+    }
+    
+    var core = App.core
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        allStudents = core.state.currentStudents.shuffled()
+        currentStudent = nil
+        tableView.rowHeight = 38
+        updateCountLabel()
+    }
+    
+    @IBAction func topViewTapped(_ sender: UITapGestureRecognizer) {
+        drawName()
     }
 
+}
+
+extension NameDrawViewController {
+    
+    func drawName() {
+        guard allStudents.count > 0 else { currentStudent = nil; return }
+        currentStudent = allStudents.removeLast()
+    }
+    
+    func handleNewStudent(_ student: Student?, previousStudent: Student?) {
+        topLabel.text = currentStudent?.displayedName ?? "Tap here to \n draw a name!"
+        if let previousStudent = previousStudent {
+            selectedStudents.append(previousStudent)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.beginUpdates()
+            tableView.insertRows(at: [indexPath], with: .top)
+            tableView.endUpdates()
+        } else if allStudents.isEmpty {
+            allStudents = core.state.currentStudents.shuffled()
+            selectedStudents.removeAll()
+            tableView.reloadData()
+            drawName()
+        }
+        updateCountLabel()
+    }
+    
+    fileprivate func updateCountLabel() {
+        let drawnNamesCount = currentStudent == nil ? selectedStudents.count : selectedStudents.count + 1
+        countLabel.text = "\(drawnNamesCount)/\(core.state.currentStudents.count)"
+    }
+    
+}
+
+extension NameDrawViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedStudents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        let studentAtRow = selectedStudents.reversed()[indexPath.row]
+        cell.textLabel?.text = studentAtRow.displayedName
+        
+        return cell
+    }
+    
 }
